a8402dfba9be9a3f37698c18e930f68b892e42af
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-12 14:34:23 +1100

    feat: add marker and allow picking locations

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,41 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState();
   // test values
   const region = {
-    lattitude: 37.78,
+    latitude: 37.78,
     longitude: -122.43,
-    lattitudeDelta: 0.0922,
+    latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
