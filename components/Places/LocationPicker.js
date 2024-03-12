ff1607d7c526a26ddc51dcce942d9df4cc03b1f1
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-12 12:26:43 +1100

    style: add styling to location picker

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,3 +1,4 @@
+import { StyleSheet } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 
 const LocationPicker = () => {
@@ -6,8 +7,8 @@ const LocationPicker = () => {
 
   return (
     <View>
-      <View></View>
-      <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
         <OutlinedButton icon="location" onPress={getLocationHandler}>
           Locate User
         </OutlinedButton>
@@ -20,3 +21,20 @@ const LocationPicker = () => {
 };
 
 export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
