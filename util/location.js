732a7a6cf67e0e31c73a2560529b2d77d34136cc
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-14 10:20:52 +1100

    feat: add getAddress util

--- util/location.js
+++ util/location.js
@@ -5,3 +5,17 @@ export function getMapPreview(lat, lng) {
 
   return imagePreviewUrl;
 }
+
+export async function getAddress(lat, lng) {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch address");
+  }
+
+  const data = response.json();
+  const address = data.results[0].formatted_address;
+  return address;
+}
